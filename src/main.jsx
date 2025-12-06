551111b066e2044f9a16b3d94ab38b3d344a4ec0
Deepak Kumar <sheetal.raghav0818@gmail.com>
2025-12-06 14:11:02 +0530

    Update Tailwind Config.

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { store } from './store';
+import React from 'react';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
